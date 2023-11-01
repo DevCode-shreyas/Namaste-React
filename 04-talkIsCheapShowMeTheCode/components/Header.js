404e60c397d62f9c054519616f41db320cf57d0b
shreyas bole <shreyasbole4@gmail.com>
2023-11-01 21:58:39 +0530

    episode 10 halfway

--- 04-talkIsCheapShowMeTheCode/components/Header.js
+++ 04-talkIsCheapShowMeTheCode/components/Header.js
@@ -11,12 +11,12 @@ const Header = () => {
   }, [btnNameReact]);
 
   return (
-    <div className="header">
+    <div className="flex justify-between">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-1" src={LOGO_URL} />
       </div>
       <div className="nav-items">
-        <ul>
+        <ul className="flex p-4 m-4">
           <li>
             <Link to="/">Home</Link>
           </li>
--- 04-talkIsCheapShowMeTheCode/src/App.js
+++ 04-talkIsCheapShowMeTheCode/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "../components/Header";
 import Body from "../components/Body";
-import "../index.css";
+import "./index.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "../components/About";
 import Contact from "../components/Contact";
